1b66ce38ce42106a7e00038be099357ee45e9341
Phodal Huang <h@phodal.com>
2022-09-27 23:39:06 +0800

    build: init keyword provides

---
+++ src/main/kotlin/com/feakin/intellij/completion/FkKeywordCompletionContributor.kt
@@ -0,0 +1,11 @@
+package com.feakin.intellij.completion
+
+import com.intellij.codeInsight.completion.CompletionContributor
+import com.intellij.openapi.project.DumbAware
+
+// refs: https://github.com/JetBrains/intellij-community/blob/master/plugins/kotlin/completion/impl-shared/src/org/jetbrains/kotlin/idea/completion/implCommon/KeywordCompletion.kt
+class FkKeywordCompletionContributor  : CompletionContributor(), DumbAware {
+    init {
+
+    }
+}
