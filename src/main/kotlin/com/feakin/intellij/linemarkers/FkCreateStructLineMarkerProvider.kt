962a48f4b62ecbb26117fdc20befb0e8c6005749
Phodal Huang <h@phodal.com>
2022-09-28 10:50:52 +0800

    feat: add line marker for struct

---
+++ src/main/kotlin/com/feakin/intellij/linemarkers/FkCreateStructLineMarkerProvider.kt
@@ -0,0 +1,23 @@
+@file:Suppress("Indentation", "ParameterListWrapping")
+
+package com.feakin.intellij.linemarkers
+
+import com.feakin.intellij.ide.editor.elementType
+import com.feakin.intellij.psi.FeakinStructDeclaration
+import com.intellij.execution.lineMarker.ExecutorAction
+import com.intellij.execution.lineMarker.RunLineMarkerContributor
+import com.intellij.icons.AllIcons
+import com.intellij.psi.PsiElement
+
+class FkCreateStructLineMarkerProvider : RunLineMarkerContributor() {
+    override fun getInfo(element: PsiElement): Info? {
+        if (element !is FeakinStructDeclaration) return null
+
+        val actions = ExecutorAction.getActions(0)
+        return Info(
+            AllIcons.RunConfigurations.TestState.Run,
+            { psiElement -> actions.mapNotNull { getText(it, psiElement) }.joinToString("\n") },
+            *actions
+        )
+    }
+}
