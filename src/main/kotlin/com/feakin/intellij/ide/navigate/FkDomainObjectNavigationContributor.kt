291dd608fa90162bd9ee8cb84577e5ea9951491a
Phodal Huang <h@phodal.com>
2022-09-29 14:53:45 +0800

    feat: try to add goto support

--- src/main/kotlin/com/feakin/intellij/ide/navigate/FkDomainObjectNavigationContributor.kt
+++ src/main/kotlin/com/feakin/intellij/ide/navigate/FkDomainObjectNavigationContributor.kt
@@ -1,24 +1,30 @@
 package com.feakin.intellij.ide.navigate
 
 import com.feakin.intellij.FkLanguage
-import com.feakin.intellij.psi.stubs.FkContextIndex
+import com.feakin.intellij.psi.FeakinContextDeclaration
+import com.feakin.intellij.psi.FeakinNamedElement
+import com.feakin.intellij.psi.stubs.FkNamedElementIndex
 import com.intellij.lang.Language
 import com.intellij.navigation.ChooseByNameContributorEx
 import com.intellij.navigation.GotoClassContributor
 import com.intellij.navigation.NavigationItem
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.stubs.StubIndex
+import com.intellij.psi.stubs.StubIndexKey
 import com.intellij.util.Processor
 import com.intellij.util.indexing.FindSymbolParameters
 import com.intellij.util.indexing.IdFilter
 
 class FkDomainObjectNavigationContributor : GotoClassContributor, ChooseByNameContributorEx {
+    private val indexKey: StubIndexKey<String, FeakinNamedElement> = FkNamedElementIndex.KEY
+
     override fun processNames(processor: Processor<in String?>, scope: GlobalSearchScope, filter: IdFilter?) {
-        if (!StubIndex.getInstance()
-                .processAllKeys(FkContextIndex.KEY, processor, scope, filter)
-        ) {
-            return
-        }
+        StubIndex.getInstance().processAllKeys(
+            indexKey,
+            processor,
+            scope,
+            null
+        )
     }
 
     override fun processElementsWithName(
@@ -26,6 +32,16 @@ class FkDomainObjectNavigationContributor : GotoClassContributor, ChooseByNameCo
         processor: Processor<in NavigationItem>,
         parameters: FindSymbolParameters
     ) {
+        StubIndex.getInstance().processElements(
+            indexKey,
+            name,
+            parameters.project,
+            parameters.searchScope,
+            null,
+            FeakinNamedElement::class.java
+        ) { element ->
+            processor.process(element)
+        }
     }
 
     override fun getQualifiedName(item: NavigationItem): String? {
