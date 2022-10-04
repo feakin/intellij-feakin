65041f30ca572b04a65b51f490c0f6478032d64f
Phodal Huang <h@phodal.com>
2022-10-05 00:29:32 +0800

    refactor(format): clean old code

--- src/main/kotlin/com/feakin/intellij/formatter/FkFormattingBlock.kt
+++ src/main/kotlin/com/feakin/intellij/formatter/FkFormattingBlock.kt
@@ -1,14 +1,12 @@
 package com.feakin.intellij.formatter
 
-import com.feakin.intellij.lexer.FeakinElementTypes.*
 import com.intellij.formatting.*
-import com.intellij.formatting.alignment.AlignmentStrategy
 import com.intellij.lang.ASTNode
 import com.intellij.openapi.util.TextRange
 import com.intellij.openapi.util.UserDataHolderBase
 import com.intellij.psi.TokenType
 import com.intellij.psi.codeStyle.CodeStyleSettings
-import java.util.*
+import com.intellij.psi.formatter.FormatterUtil
 
 open class FkFormattingBlock(
     private val myNode: ASTNode,
@@ -42,50 +40,14 @@ open class FkFormattingBlock(
     private val mySubBlocks: List<Block> by lazy { buildChildren() }
 
     private fun buildChildren(): List<Block> {
-        val strategy: AlignmentStrategy.AlignmentPerTypeStrategy? = null
-        val blocks = mutableListOf<Block>()
-        var child: ASTNode? = myNode.firstChildNode
-        while (child != null) {
-            val childType = child.elementType
-            if (child.textRange.length == 0) {
-                child = child.treeNext
-                continue
+        val toCollection = node.getChildren(null)
+            .filter { !it.isWhitespaceOrEmpty() }
+            .map { childNode: ASTNode ->
+                childNode.elementType
             }
-            if (childType === TokenType.WHITE_SPACE) {
-                child = child.treeNext
-                continue
-            }
+            .toCollection(ArrayList())
 
-            val substitutor = if (childType === BLOCK_COMMENT) COMMENT else childType
-            val alignment = strategy?.getAlignment(substitutor)
-            blocks.addAll(buildSubBlocks(child, alignment))
-            child = child.treeNext
-        }
-        return Collections.unmodifiableList(blocks)
-    }
-
-    private fun buildSubBlocks(child: ASTNode, alignment: Alignment?): List<FkFormattingBlock> {
-        // DECLARATION
-        val children = child.getChildren(null)
-        if (children.isEmpty()) {
-            return listOf(FkFormattingBlock(child, alignment, Indent.getNoneIndent(), null, mySettings, spacingBuilder))
-        }
-
-        // all children DECLARATION
-        val decl = child.firstChildNode
-        decl?.elementType
-            ?: return listOf(FkFormattingBlock(child, alignment, Indent.getNoneIndent(), null, mySettings, spacingBuilder))
-
-        // declaration with children and body
-//        val blocks = decl.getChildren(null)
-//            .filter {
-//                it.elementType == CONTEXT_MAP_BODY || it.elementType == CONTEXT_BODY || it.elementType == ENDPOINT_BODY || it.elementType == IMPL_BODY
-//                        || it.elementType == REQUEST_BODY
-//            }
-//            .map {
-//                val indent = Indent.getNormalIndent()
-//                FkFormattingBlock(child, alignment, indent, null, mySettings, spacingBuilder)
-//            }.toCollection(mutableListOf())
+        println(toCollection)
 
         return listOf()
     }
@@ -95,22 +57,15 @@ open class FkFormattingBlock(
     }
 
     override fun getChildAttributes(newChildIndex: Int): ChildAttributes {
-        val childIndent = Indent.getNoneIndent()
-
-        val parentType = myNode.elementType
-        if (parentType == CONTEXT_MAP_BODY || parentType == CONTEXT_BODY || parentType == ENDPOINT_BODY || parentType == IMPL_BODY
-            || parentType == REQUEST_BODY
-        ) {
-            return ChildAttributes(Indent.getNormalIndent(), null)
-        }
-
-        return ChildAttributes(childIndent, null)
+        return ChildAttributes(Indent.getNoneIndent(), null)
     }
 
-    override fun isIncomplete(): Boolean = false
-//    private val myIsIncomplete: Boolean by lazy { FormatterUtil.isIncomplete(node) }
+    override fun isIncomplete(): Boolean = myIsIncomplete
+    private val myIsIncomplete: Boolean by lazy { FormatterUtil.isIncomplete(node) }
 
-    override fun isLeaf(): Boolean = false
+    override fun isLeaf(): Boolean = node.firstChildNode == null
 
     override fun toString() = "${node.text} $textRange"
 }
+
+fun ASTNode?.isWhitespaceOrEmpty() = this == null || textLength == 0 || elementType == TokenType.WHITE_SPACE
