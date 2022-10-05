44200aa6640fcc5ab5719f855b2eacefbdec701d
Phodal Huang <h@phodal.com>
2022-10-06 00:57:22 +0800

    refactor: replace internal API

--- src/main/kotlin/com/feakin/intellij/runconfig/FkCommandRunner.kt
+++ src/main/kotlin/com/feakin/intellij/runconfig/FkCommandRunner.kt
@@ -1,19 +1,18 @@
 package com.feakin.intellij.runconfig
 
-import com.intellij.execution.ExecutionManager
 import com.intellij.execution.ExecutionResult
 import com.intellij.execution.configurations.RunProfile
+import com.intellij.execution.configurations.RunProfileState
 import com.intellij.execution.configurations.RunnerSettings
 import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.execution.runners.ExecutionEnvironment
-import com.intellij.execution.runners.ProgramRunner
+import com.intellij.execution.runners.GenericProgramRunner
 import com.intellij.execution.ui.ExecutionUiService
 import com.intellij.execution.ui.RunContentDescriptor
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import org.jetbrains.annotations.NonNls
-import org.jetbrains.concurrency.resolvedPromise
 
-class FkCommandRunner : ProgramRunner<RunnerSettings> {
+class FkCommandRunner : GenericProgramRunner<RunnerSettings>() {
     override fun getRunnerId(): @NonNls String = "FkCommandRunner"
 
     override fun canRun(executorId: String, profile: RunProfile): Boolean {
@@ -21,15 +20,7 @@ class FkCommandRunner : ProgramRunner<RunnerSettings> {
         return true
     }
 
-    override fun execute(environment: ExecutionEnvironment) {
-        val state = environment.state ?: return
-        @Suppress("UnstableApiUsage")
-        ExecutionManager.getInstance(environment.project).startRunProfile(environment) {
-            resolvedPromise(doExecute((state as FkRunState), environment))
-        }
-    }
-
-    private fun doExecute(state: FkRunState, environment: ExecutionEnvironment): RunContentDescriptor? {
+    override fun doExecute(state: RunProfileState, environment: ExecutionEnvironment): RunContentDescriptor? {
         val configuration = environment.runProfile
         if (configuration is FkCommandConfiguration) {
             FileDocumentManager.getInstance().saveAllDocuments()
