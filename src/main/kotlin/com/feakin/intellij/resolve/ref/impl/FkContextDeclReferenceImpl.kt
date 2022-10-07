6ffeab49dc516f5093b5d0c5a3f0f06b98e66dc3
Phodal Huang <h@phodal.com>
2022-10-07 13:37:13 +0800

    feat(usage): add cached for reference

--- src/main/kotlin/com/feakin/intellij/resolve/ref/impl/FkContextDeclReferenceImpl.kt
+++ src/main/kotlin/com/feakin/intellij/resolve/ref/impl/FkContextDeclReferenceImpl.kt
@@ -4,45 +4,20 @@ import com.feakin.intellij.FkFile
 import com.feakin.intellij.FkFileType
 import com.feakin.intellij.psi.*
 import com.feakin.intellij.resolve.ref.FkReferenceBase
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Key
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiManager
 import com.intellij.psi.search.FileTypeIndex
 import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.util.*
 
 class FkContextDeclReferenceImpl(
     element: FkContextDeclaration
 ) : FkReferenceBase<FkContextDeclaration>(element) {
     override fun multiResolve(): List<FkElement> {
-        val project = element.project
-        val scope = GlobalSearchScope.allScope(project)
-        val collection = mutableListOf<FkElement>()
-
-        val virtualFiles: Collection<VirtualFile> = FileTypeIndex.getFiles(FkFileType, scope)
-        virtualFiles.forEach { virtualFile ->
-            val fkFile = PsiManager.getInstance(project).findFile(virtualFile) as FkFile
-            val fkDeclarations = PsiTreeUtil.getChildrenOfType(fkFile, FkDeclaration::class.java)
-
-            val contextMaps: Array<out FkContextMapDeclaration>? = fkDeclarations?.mapNotNull {
-                PsiTreeUtil.getChildrenOfType(it, FkContextMapDeclaration::class.java)?.toList()
-            }?.flatten()?.toTypedArray()
-
-            val mapBodys: Array<out FkContextMapBody>? = contextMaps?.mapNotNull {
-                PsiTreeUtil.getChildrenOfType(it, FkContextMapBody::class.java)?.toList()
-            }?.flatten()?.toTypedArray()
-
-            mapBodys?.forEach { contextMapDeclaration ->
-                PsiTreeUtil.getChildrenOfType(contextMapDeclaration, FkContextName::class.java)
-                    ?.forEach { contextName ->
-                        if (contextName.text == element.name) {
-                            collection.add(contextName)
-                        }
-                    }
-            }
-        }
-
-        return collection
+        return element.inference
     }
 
     override fun isReferenceTo(element: PsiElement): Boolean {
@@ -50,3 +25,47 @@ class FkContextDeclReferenceImpl(
     }
 }
 
+
+private val CONTEXT_NAME_INFERENCE_KEY: Key<CachedValue<List<FkElement>>> = Key.create("CONTEXT_NAME_INFERENCE_KEY")
+
+typealias InferenceResult = List<FkElement>;
+
+val FkElement.inference: InferenceResult
+    get() = CachedValuesManager.getCachedValue(this, CONTEXT_NAME_INFERENCE_KEY) {
+        CachedValueProvider.Result.create(
+            inferContextName(this as FkNamedElement),
+            PsiModificationTracker.MODIFICATION_COUNT
+        )
+    }
+
+private fun inferContextName(
+    element: FkNamedElement
+): InferenceResult {
+    val project = element.project
+    val scope = GlobalSearchScope.allScope(project)
+    val collection = mutableListOf<FkElement>()
+
+    val virtualFiles: Collection<VirtualFile> = FileTypeIndex.getFiles(FkFileType, scope)
+    virtualFiles.forEach { virtualFile ->
+        val fkFile = PsiManager.getInstance(project).findFile(virtualFile) as FkFile
+        val fkDeclarations = PsiTreeUtil.getChildrenOfType(fkFile, FkDeclaration::class.java)
+
+        val contextMaps: Array<out FkContextMapDeclaration>? = fkDeclarations?.mapNotNull {
+            PsiTreeUtil.getChildrenOfType(it, FkContextMapDeclaration::class.java)?.toList()
+        }?.flatten()?.toTypedArray()
+
+        val mapBodys: Array<out FkContextMapBody>? = contextMaps?.mapNotNull {
+            PsiTreeUtil.getChildrenOfType(it, FkContextMapBody::class.java)?.toList()
+        }?.flatten()?.toTypedArray()
+
+        mapBodys?.forEach { contextMapDeclaration ->
+            PsiTreeUtil.getChildrenOfType(contextMapDeclaration, FkContextName::class.java)
+                ?.forEach { contextName ->
+                    if (contextName.text == element.name) {
+                        collection.add(contextName)
+                    }
+                }
+        }
+    }
+    return collection
+}
