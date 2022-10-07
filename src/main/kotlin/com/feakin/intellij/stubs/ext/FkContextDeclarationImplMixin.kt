9682067b3456e242cfcc8704375851cfd71f1480
Phodal Huang <h@phodal.com>
2022-10-07 14:28:36 +0800

    fix: fix some warning

--- src/main/kotlin/com/feakin/intellij/stubs/ext/FkContextDeclarationImplMixin.kt
+++ src/main/kotlin/com/feakin/intellij/stubs/ext/FkContextDeclarationImplMixin.kt
@@ -4,19 +4,19 @@ import com.feakin.intellij.psi.FkContextDeclaration
 import com.feakin.intellij.psi.FkStubbedNamedElementImpl
 import com.feakin.intellij.resolve.ref.FkReference
 import com.feakin.intellij.resolve.ref.impl.FkContextDeclReferenceImpl
-import com.feakin.intellij.resolve.ref.impl.FkContextNameReferenceImpl
 import com.feakin.intellij.stubs.FkContextDeclStub
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
 import com.intellij.psi.stubs.IStubElementType
 
-abstract class FkContextDeclMixin : FkStubbedNamedElementImpl<FkContextDeclStub>, FkContextDeclaration {
+abstract class FkContextDeclarationImplMixin : FkStubbedNamedElementImpl<FkContextDeclStub>, FkContextDeclaration {
     constructor(node: ASTNode) : super(node)
 
     constructor(stub: FkContextDeclStub, nodeType: IStubElementType<*, *>) : super(stub, nodeType)
 
     override fun getReference(): FkReference = FkContextDeclReferenceImpl(this)
 
-    override val referenceNameElement: PsiElement get() = identifier
+    override val referenceNameElement: PsiElement get() = identifier ?: this
+
     override val referenceName: String get() = greenStub?.name ?: super.referenceName
 }
