130f5e907966daa454602da775ba468bbe59b0ff
Phodal Huang <h@phodal.com>
2022-10-19 21:35:13 +0800

    refactor: change command line

--- src/main/kotlin/com/feakin/intellij/runconfig/config/FkCommandLine.kt
+++ src/main/kotlin/com/feakin/intellij/runconfig/config/FkCommandLine.kt
@@ -3,18 +3,20 @@ package com.feakin.intellij.runconfig.config
 class FkCommandLine(
     var path: String,
     var impl: String,
-    val subcommand: String,
-    val funcName: String = "",
+    private val subcommand: String,
+    private val funcName: String = "",
 ) {
     fun toCommandString(): String {
         return this.toCommand().joinToString(" ")
     }
 
     fun toCommand(): List<String> {
+        val cmdList = listOf("fkl", subcommand, "--path", path, "--impl", impl)
+
         if (subcommand == "run") {
-            return listOf("fkl", subcommand, "--path", path, "--impl", impl, "--func", funcName)
+            cmdList.containsAll(listOf("--func", funcName))
         }
 
-        return listOf("fkl", subcommand, "--path", path, "--impl", impl)
+        return cmdList
     }
 }
