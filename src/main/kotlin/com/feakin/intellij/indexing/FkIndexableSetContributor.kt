e2bf15c98e20f7e939170f6cfc3472acbf818983
Phodal Huang <h@phodal.com>
2022-10-22 14:27:18 +0800

    refactor: remove unused code

--- src/main/kotlin/com/feakin/intellij/indexing/FkIndexableSetContributor.kt
+++
@@ -1,9 +0,0 @@
-package com.feakin.intellij.indexing
-
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.util.indexing.IndexableSetContributor
-
-class FkIndexableSetContributor : IndexableSetContributor() {
-    override fun getAdditionalRootsToIndex(): MutableSet<VirtualFile> = mutableSetOf()
-}
