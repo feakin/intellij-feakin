e4edb10e7235b729536f4ed7988964f7142f7e6c
Phodal Huang <h@phodal.com>
2022-10-26 21:25:32 +0800

    refactor: rename variable to align API

--- src/main/kotlin/com/feakin/intellij/inspections/lints/FkDuplicateDeclInspection.kt
+++ src/main/kotlin/com/feakin/intellij/inspections/lints/FkDuplicateDeclInspection.kt
@@ -1,9 +1,6 @@
 package com.feakin.intellij.inspections.lints
 
-import com.feakin.intellij.psi.FkAggregateDeclaration
-import com.feakin.intellij.psi.FkContextDeclaration
-import com.feakin.intellij.psi.FkEntityDeclaration
-import com.feakin.intellij.psi.FkVisitor
+import com.feakin.intellij.psi.*
 import com.feakin.intellij.resolve.indexes.FkNamedElementIndex
 import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.ProblemsHolder
@@ -15,34 +12,37 @@ class FkDuplicateDeclInspection : LocalInspectionTool() {
         return FkDuplicateContextDeclVisitor(holder)
     }
 
-    private class FkDuplicateContextDeclVisitor(val holder: ProblemsHolder): FkVisitor() {
-        override fun visitContextDeclaration(decl: FkContextDeclaration) {
-            decl.identifier.let { name ->
-                FkNamedElementIndex.findElementsByName(name.text, decl.project).filterIsInstance<FkContextDeclaration>().let { decls ->
-                    if (decls.size > 1) {
-                        holder.registerProblem(decl, "Duplicate context declaration")
+    private class FkDuplicateContextDeclVisitor(val holder: ProblemsHolder) : FkVisitor() {
+        override fun visitContextDeclaration(o: FkContextDeclaration) {
+            o.identifier.let { name ->
+                FkNamedElementIndex.findElementsByName(name.text, o.project).filterIsInstance<FkContextDeclaration>()
+                    .let { decls ->
+                        if (decls.size > 1) {
+                            holder.registerProblem(o, "Duplicate context declaration")
+                        }
                     }
-                }
             }
         }
 
         override fun visitAggregateDeclaration(o: FkAggregateDeclaration) {
             o.identifier.let { name ->
-                FkNamedElementIndex.findElementsByName(name.text, o.project).filterIsInstance<FkAggregateDeclaration>().let { decls ->
-                    if (decls.size > 1) {
-                        holder.registerProblem(o, "Duplicate aggregate declaration")
+                FkNamedElementIndex.findElementsByName(name.text, o.project).filterIsInstance<FkAggregateDeclaration>()
+                    .let { decls ->
+                        if (decls.size > 1) {
+                            holder.registerProblem(o, "Duplicate aggregate declaration")
+                        }
                     }
-                }
             }
         }
 
         override fun visitEntityDeclaration(o: FkEntityDeclaration) {
             o.identifier.let { name ->
-                FkNamedElementIndex.findElementsByName(name.text, o.project).filterIsInstance<FkEntityDeclaration>().let { decls ->
-                    if (decls.size > 1) {
-                        holder.registerProblem(o, "Duplicate entity declaration")
+                FkNamedElementIndex.findElementsByName(name.text, o.project).filterIsInstance<FkEntityDeclaration>()
+                    .let { decls ->
+                        if (decls.size > 1) {
+                            holder.registerProblem(o, "Duplicate entity declaration")
+                        }
                     }
-                }
             }
         }
     }
