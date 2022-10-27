b425924edbfd6e177331f87eadc265e623286778
Phodal Huang <h@phodal.com>
2022-10-27 09:28:02 +0800

    feat: add color process handler output

--- src/main/kotlin/com/feakin/intellij/runconfig/FkProcessHandler.kt
+++ src/main/kotlin/com/feakin/intellij/runconfig/FkProcessHandler.kt
@@ -3,17 +3,14 @@ package com.feakin.intellij.runconfig
 import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.execution.configurations.PtyCommandLine
 import com.intellij.execution.process.AnsiEscapeDecoder
+import com.intellij.execution.process.KillableColoredProcessHandler
 import com.intellij.execution.process.KillableProcessHandler
 import com.intellij.openapi.util.Key
 import com.intellij.util.io.BaseDataReader
 import com.intellij.util.io.BaseOutputReader
 
-class FkProcessHandler(commandLine: GeneralCommandLine) : KillableProcessHandler(commandLine),
+class FkProcessHandler(commandLine: GeneralCommandLine) : KillableColoredProcessHandler(commandLine),
     AnsiEscapeDecoder.ColoredTextAcceptor {
-    override fun coloredTextAvailable(text: String, attributes: Key<*>) {
-        super.notifyTextAvailable(text, attributes)
-    }
-
     init {
         setHasPty(commandLine is PtyCommandLine)
         setShouldDestroyProcessRecursively(!hasPty())
