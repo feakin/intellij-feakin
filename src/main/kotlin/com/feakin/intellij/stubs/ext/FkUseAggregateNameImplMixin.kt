56513cb332df6e4af5f4a4050ed8da9b708d7ad4
Phodal Huang <h@phodal.com>
2022-11-05 22:34:28 +0800

    feat: add support for aggregate name suport

---
+++ src/main/kotlin/com/feakin/intellij/stubs/ext/FkUseAggregateNameImplMixin.kt
@@ -0,0 +1,16 @@
+package com.feakin.intellij.stubs.ext
+
+import com.feakin.intellij.psi.FkUseAggregateName
+import com.feakin.intellij.psi.impl.FkElementImpl
+import com.feakin.intellij.resolve.ref.FkReference
+import com.feakin.intellij.resolve.ref.impl.FkUseAggregateNameReferenceImpl
+import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiElement
+
+abstract class FkUseAggregateNameImplMixin(node: ASTNode) : FkElementImpl(node), FkUseAggregateName {
+    override val referenceName: String get() = referenceNameElement.text
+
+    override val referenceNameElement: PsiElement get() = identifier
+
+    override fun getReference(): FkReference = FkUseAggregateNameReferenceImpl(this)
+}
