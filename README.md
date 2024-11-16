e8e86dc19becb25795c48c58a9fca6022f95c76a
Phodal Huang <gmszone@qq.com>
2024-11-16 20:21:52 +0800

    chore: update intellij gradle to 2

--- README.md
+++ README.md
@@ -4,13 +4,6 @@
 [![Version](https://img.shields.io/jetbrains/plugin/v/20026-feakin.svg)](https://plugins.jetbrains.com/plugin/20026-feakin)
 [![Downloads](https://img.shields.io/jetbrains/plugin/d/20026-feakin.svg)](https://plugins.jetbrains.com/plugin/20026-feakin)
 
-<!-- Plugin description -->
-Feakin is an architecture DSL for design, governance and visualization, based on Diagram-as-code. It can highlight code, generate structure, and others.
-
-Fklang 是一个基于软件开发工业化思想设计的架构设计 DSL。通过显性化软件架构设计，以确保软件系统描述与实现的一致性。并在工作流中，内嵌对于 AI 代码生成软件的支持，以构筑完整的开发者体验。
-
-<!-- Plugin description end -->
-
 Features:
 
 - [x] Lexer and Parser Definition
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,19 @@
-import org.jetbrains.changelog.markdownToHTML
 import org.gradle.api.JavaVersion.VERSION_17
 import org.jetbrains.changelog.Changelog
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-fun properties(key: String) = project.findProperty(key).toString()
+fun properties(key: String) = providers.gradleProperty(key)
 
 plugins {
     idea
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.8.22"
+    id("org.jetbrains.kotlin.jvm") version "2.0.21"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.17.4"
+    id("org.jetbrains.intellij.platform") version "2.1.0"
     // Gradle Changelog Plugin
-    id("org.jetbrains.changelog") version "2.1.2"
+    id("org.jetbrains.changelog") version "2.2.1"
     // Gradle Qodana Plugin
     id("org.jetbrains.qodana") version "0.1.13"
 
@@ -27,16 +26,28 @@ version = properties("pluginVersion")
 // Configure project's dependencies
 repositories {
     mavenCentral()
+
+    intellijPlatform {
+        defaultRepositories()
+        jetbrainsRuntime()
+    }
 }
 
-// Configure Gradle IntelliJ Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    pluginName.set(properties("pluginName"))
-    version.set(properties("platformVersion"))
-    type.set(properties("platformType"))
+intellijPlatform {
+    pluginConfiguration {
+        id = properties("pluginName")
+        name = properties("platformVersion")
+        version = properties("pluginVersion")
+    }
+}
 
-    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
-    plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
+// Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
+changelog {
+    version.set(properties("pluginVersion"))
+    groups.empty()
+    path.set(rootProject.file("CHANGELOG.md").toString())
+    repositoryUrl.set(properties("pluginRepositoryUrl"))
+    itemPrefix.set("*")
 }
 
 configure<JavaPluginExtension> {
@@ -116,38 +127,28 @@ tasks {
     }
 
     patchPluginXml {
-        version.set(properties("pluginVersion"))
-        sinceBuild.set(properties("pluginSinceBuild"))
-        untilBuild.set(properties("pluginUntilBuild"))
+        pluginDescription.set(provider { file("src/description.html").readText() })
 
-        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
-        pluginDescription.set(
-            projectDir.resolve("README.md").readText().lines().run {
-                val start = "<!-- Plugin description -->"
-                val end = "<!-- Plugin description end -->"
+        changelog {
+            version.set(properties("pluginVersion"))
+            groups.empty()
+            path.set(rootProject.file("CHANGELOG.md").toString())
+            repositoryUrl.set(properties("pluginRepositoryUrl"))
+        }
 
-                if (!containsAll(listOf(start, end))) {
-                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
-                }
-                subList(indexOf(start) + 1, indexOf(end))
-            }.joinToString("\n").run { markdownToHTML(this) }
-        )
-
+        val changelog = project.changelog
         // Get the latest available change notes from the changelog file
-        changeNotes.set(provider {
-            changelog.renderItem(changelog.run {
-                getOrNull(properties("pluginVersion")) ?: getLatest()
-            }, Changelog.OutputType.HTML)
-        })
-    }
+        changeNotes.set(properties("pluginVersion").map { pluginVersion ->
+            with(changelog) {
+                renderItem(
+                    (getOrNull(pluginVersion) ?: getUnreleased())
+                        .withHeader(false)
+                        .withEmptySections(false),
 
-    // Configure UI tests plugin
-    // Read more: https://github.com/JetBrains/intellij-ui-test-robot
-    runIdeForUiTests {
-        systemProperty("robot-server.port", "8082")
-        systemProperty("ide.mac.message.dialogs.as.sheets", "false")
-        systemProperty("jb.privacy.policy.text", "<!--999.999-->")
-        systemProperty("jb.consents.confirmation.enabled", "false")
+                    Changelog.OutputType.HTML,
+                )
+            }
+        })
     }
 
     signPlugin {
@@ -159,13 +160,5 @@ tasks {
     publishPlugin {
         dependsOn("patchChangelog")
         token.set(System.getenv("PUBLISH_TOKEN"))
-        // pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
-        // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
-        // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
-        val pluginVersion = properties("pluginVersion")
-        val channel: String = pluginVersion.split('-').getOrElse(1) { "default" }.split('.')[0]
-        val channelProvider: Provider<List<String>> = providers.provider { listOf(channel) }
-
-        channels.set(channelProvider)
     }
 }
